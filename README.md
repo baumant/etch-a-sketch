498407993618426ec31f85ed0cc2cc4919226d93
Tim Bauman <tebauman@gmail.com>
2015-05-14 22:00:31 -0400

    Initial Project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etch-a-sketch
 Draw on your screen etch a sketch powered by jquery
+
+Click "New Grid" to erase your drawing and create a new one
+Click "Random Colors" for a colorful brush!
+Click "Watercolor" for a gradually darker brush
---
+++ js.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+    var dim = 16;
+    var box = "<div class='box'></div>";
+
+    function createGrid() {
+        for (var x = 1; x <= (dim * dim); x++) {
+            if (x % dim === 0) {
+                $('.container').append(box + '<br>');
+            } else {
+                $('.container').append(box);
+            };
+        };
+    };
+
+    createGrid();
+
+    $('.box').mouseenter(function () {
+        $(this).css('background-color', 'black');
+    });
+
+    $('#newgrid').click(function () {
+        $('.container').empty();
+
+        dim = prompt("How many blocks per side?  (1 - 50)");
+
+        var boxD = 512 / dim;
+
+        createGrid();
+
+        $('.box').width(boxD);
+        $('.box').height(boxD);
+
+        $('.box').mouseenter(function () {
+            $(this).css('background-color', '#000000');
+        });
+    });
+
+    $('#colors').click(function () {
+        $('.container').empty();
+
+        dim = prompt("How many blocks per side?  (1 - 50)");
+
+        var boxD = 512 / dim;
+
+        createGrid();
+
+        $('.box').width(boxD);
+        $('.box').height(boxD);
+
+        $('.box').mouseenter(function () {
+            var randColor = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
+            $(this).css('background-color', randColor);
+        });
+    });
+
+    $('#watercolor').click(function () {
+        $('.container').empty();
+
+        dim = prompt("How many blocks per side?  (1 - 50)");
+
+        var boxD = 512 / dim;
+
+        createGrid();
+        
+        $('.box').width(boxD);
+        $('.box').height(boxD);
+        $('.box').css('background-color', 'black');
+        $('.box').css('opacity', '0');
+
+        $('.box').mouseenter(function () {
+            var o = $(this).css("opacity");
+            $(this).css('opacity', '+=.1');
+        });
+    });
+});
